79157ec348de1e3f30aa9a8cc7dfec622c849355
Gene Boggs <gene@ology.net>
2025-09-06 12:08:15 -0700

    WIP

--- src/music_drummer/music_drummer.py
+++ src/music_drummer/music_drummer.py
@@ -3,13 +3,14 @@ from music21 import stream, note, tempo, meter, duration
 class Drummer:
     STRAIGHT = 50
 
-    def __init__(self, file='Drummer.mid', bpm=120, volume=100, signature='4/4', bars=4):
+    def __init__(self, file='Drummer.mid', bpm=120, volume=100, accent=20, signature='4/4', bars=4):
         self.file = file
         self.bpm = bpm
         self.volume = volume
         self.bars = bars
         self.counter = 0
         self.channel = 9
+        self.accent = accent
         self.signature = signature
         self._init_score()
 
@@ -38,3 +39,17 @@ class Drummer:
         self.score.append(n)
         self.counter += dur
         return n
+    
+    def accent_note(self, pitch, dur=1.0, volume=None, accent=None):
+        if volume is None:
+            volume = self.volume
+        if accent is None:
+            accent = self.accent
+        self.note(pitch, dur, volume + accent)
+    
+    def duck_note(self, pitch, dur=1.0, volume=None, accent=None):
+        if volume is None:
+            volume = self.volume
+        if accent is None:
+            accent = self.accent
+        self.note(pitch, dur, volume - accent)
