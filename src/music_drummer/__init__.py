088f22989805c647939cfae5c5c36c031853fbb3
Gene Boggs <gene@ology.net>
2025-09-08 16:20:33 -0700

    WIP

--- src/music_drummer/__init__.py
+++ src/music_drummer/__init__.py
@@ -4,4 +4,4 @@ from music_drummer.music_drummer import Drummer
 # This is the version number of your code.  This will change each time you make
 # a new release.  The most popular approach is to use Semantic Versioning,
 # described at https://semver.org/
-__version__ = "0.1.1"
+__version__ = "0.1.2"
