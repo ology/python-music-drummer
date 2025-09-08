f2545255a3e9b12c30ecfe5a9ffa311d2f77d5ec
Gene Boggs <gene@ology.net>
2025-09-08 11:09:35 -0700

    WIP

--- tests.py
+++ tests.py
@@ -79,9 +79,9 @@ class TestDrummer(unittest.TestCase):
 
     def test_flam(self):
         d = Drummer()
-        d.note(d.instruments['snare']['num'], dur=1/2, flam=False, part=d.snare)
+        d.note(d.instruments['snare']['num'], dur=1/2, flam=0, part=d.snare)
         d.note(d.instruments['snare']['num'], dur=1/2, flam=1/16, part=d.snare)
-        d.note(d.instruments['snare']['num'], dur=1/2, flam=False, part=d.snare)
+        d.note(d.instruments['snare']['num'], dur=1/2, flam=0, part=d.snare)
         self.assertEqual(len(d.snare.getElementsByClass('Note')), 4)
         self.assertEqual(d.snare.getElementsByClass('Note')[1].duration.quarterLength, 1/16)
         self.assertEqual(d.snare.getElementsByClass('Note')[2].duration.quarterLength, 1/2 - 1/16)
