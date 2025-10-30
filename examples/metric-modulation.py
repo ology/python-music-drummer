bdb88e03ddb4dd7c95b58e73d75003e4d5fea706
Gene Boggs <gene@ology.net>
2025-10-30 15:14:34 -0700

    WIP

--- examples/metric-modulation.py
+++ examples/metric-modulation.py
@@ -1,3 +1,5 @@
+# XXX currently not working :\
+
 try:
     import sys
     sys.path.append('./src')
@@ -6,6 +8,7 @@ except ImportError:
     from music_drummer import Drummer
 
 bpm = sys.argv[1] if len(sys.argv) > 1 else 70
+ql = 1
 
 d = Drummer()
 d.set_ts()
@@ -13,16 +16,10 @@ d.set_bpm(bpm)
 
 d.count_in()
 
-for i in range(1,4):
+for i in range(4):
+    if i > 0:
+        bpm /= 2
     d.set_bpm(bpm)
-    d.pattern(
-        patterns={
-            'kick':  '1000000010100000',
-            'snare': '0000100000001000',
-            'hihat': '0010101010101010',
-        }
-    )
-    d.set_bpm(bpm * 1/3)
     d.pattern(
         patterns={
             'kick':  '1000000010100000',
